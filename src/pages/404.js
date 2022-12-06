2b95e890368aee2c91f933d645f165c3881fa912
tomato <h20333@qq.com>
2022-12-06 20:51:47 +0800

    text:测试上传是否正常

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
   return (
     <Result
       status="404"
-      title="404 NotFound"
+      title="404"
       subTitle="Sorry, the page you visited does not exist."
       extra={
         <Button type="primary" onClick={() => navigate("/")}>
