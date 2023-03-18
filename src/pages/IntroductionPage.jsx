e0219e2d0575ddd44b56a926c0deb3f0bf7de020
tomato <h20333@qq.com>
2023-03-18 10:35:38 +0800

    fix重构首页

--- src/pages/IntroductionPage.jsx
+++ src/pages/IntroductionPage.jsx
@@ -1,125 +1,137 @@
-import { Button, Space, Card } from "antd";
+import { Button, Space, Col, Row } from "antd";
 import { useNavigate } from "react-router-dom";
 
-import backGroundImage from "../assets/backgrounfImage.png";
-const title = {
-  fontSize: "2.7rem",
-  textAlign: "center",
-  marginTop: "8vh",
+import backGround from "../assets/background.png";
+import backGroundDown from "../assets/backGroundDown.png";
+
+const backGroundCss = {
+  backgroundImage: `url(${backGround})`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  height: "50vh",
+  width: "100vw",
+};
+// 偏左侧布局
+const TitleTextBox = {
+  padding: "40px",
+  textAlign: "left",
+  marginTop: "40px",
+};
+// 标题变大
+const TitleText = {
+  fontSize: "2.2rem",
   fontWeight: "bolder",
 };
-const introduntion = {
-  textAlign: "center",
-  fontSize: "1.3rem",
-  fontWeight: "lighter",
-};
-const firstIntroduntion = {
-  textAlign: "center",
-  fontSize: "1.3rem",
-  fontWeight: "lighter",
-  marginTop: "4vh",
-};
-const letsBegin = {
-  marginLeft: "50%",
-  marginTop: "30px",
-  transform: "translate(-50%)",
-};
+
 const buttonStyle = {
-  marginRight: "30px",
+  marginTop: "10px",
 };
 
-const CardStyle = {
-  marginTop: "70px",
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
+const backGroundDownCss = {
+  backgroundImage: `url(${backGroundDown})`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  height: "50vh",
+  width: "100vw",
 };
-const CardShadow = {
-  width: 250,
-  height: 110,
+const listCss = {
+  fontWeight: "lighter",
+  textAlign: "left",
+  marginTop: "30px",
 };
+
 function IntroductionPage() {
   const navigate = useNavigate();
   const LinkToHome = () => {
     navigate("/home");
   };
+
   return (
     <>
-      <img
-        src={backGroundImage}
-        alt="图片无法显示"
-        style={{ width: "100%", height: "23vh" }}
-      />
-      <h1 style={title}>计量语言学指标提取工具 1.0 </h1>
-      <p style={firstIntroduntion}>
-        本网站是面向计量语言学学科构建的指标提取工具，并且面向多个不同的语种
-      </p>
-      <p style={introduntion}>
-        对于输入的语言文本，
-        网站能够识别不同的语种，并且能够根据不同语种进行计量指标的提取
-      </p>
-      <p style={introduntion}>
-        用于辅助计量语言学的研究， 为计量语言学相关的学者和研究人员提供帮助
-      </p>
-      <Space wrap style={letsBegin}>
-        <Button
-          type="primary"
-          style={buttonStyle}
-          size="large"
-          onClick={LinkToHome}
-        >
-          开始使用
-        </Button>
-        <Button size="large">了解详情</Button>
-      </Space>
+      {/* 上方 */}
+      <div style={backGroundCss}>
+        <Row>
+          <Col span={18} offset={5}>
+            <div style={TitleTextBox}>
+              <h1 style={TitleText}>计量指标提取工具 1.0 </h1>
+              <h1 style={TitleText}>面向计量语言学学科构建的指标提取工具 </h1>
 
-      <div>
-        <Space style={CardStyle}>
-          <Card
-            title="汉语"
-            style={CardShadow}
-            extra={<a href="/#/home">More</a>}
-          >
-            <p>通用计量指标提取</p>
-            <p>汉语计量指标提取</p>
-          </Card>
+              <div
+                style={{
+                  textAlign: "left",
+                  marginTop: "30px",
+                  fontSize: "12px",
+                  fontWeight: "lighter",
+                }}
+              >
+                <p>
+                  面向多个不同的语种, 对于输入的语言文本，
+                  网站能够识别不同的语种，并且能够根据不同语种切换计量指标的提取
+                </p>
+                <p>
+                  用于辅助计量语言学的研究，为计量语言学相关的学者和研究人员提供帮助
+                </p>
+              </div>
 
-          <Card
-            title="英语"
-            style={CardShadow}
-            extra={<a href="/#/home">More</a>}
-          >
-            <p>通用计量指标提取</p>
-            <p>英语可读性指标提取</p>
-          </Card>
+              <Space style={buttonStyle}>
+                <Button
+                  type="primary"
+                  ghost
+                  size="middle"
+                  style={{ marginRight: "10px" }}
+                  onClick={LinkToHome}
+                >
+                  开始使用
+                </Button>
+                <Button size="middle" type="primary" danger ghost>
+                  了解详情
+                </Button>
+              </Space>
+            </div>
+          </Col>
+        </Row>
+      </div>
 
-          <Card
-            title="日语"
-            style={CardShadow}
-            extra={<a href="/#/home">More</a>}
-          >
-            <p>通用计量指标提取</p>
-            <p>Card content</p>
-          </Card>
-
-          <Card
-            title="印尼语"
-            style={CardShadow}
-            extra={<a href="/#/home">More</a>}
-          >
-            <p>通用计量指标提取</p>
-            <p>Card content</p>
-          </Card>
-
-          <Card
-            title="菲律宾语"
-            style={CardShadow}
-            extra={<a href="/#/home">More</a>}
-          >
-            <p>通用计量指标提取</p>
-            <p>Card content</p>
-          </Card>
-        </Space>
+      {/* 下方 */}
+      <div style={backGroundDownCss}>
+        <Row>
+          <Col span={8} offset={5}>
+            <div style={TitleTextBox}>
+              <h1 style={TitleText}>指标提取工具可以做什么 </h1>
+              <p style={{ fontSize: "12px", fontWeight: "lighter" }}>
+                输入文本，语种识别，切换语种，提取文本中的计量指标。
+              </p>
+              <p style={{ fontSize: "12px", fontWeight: "lighter" }}>
+                上传文档，语种识别，提取文档中的计量指标。
+              </p>
+            </div>
+          </Col>
+          <Col span={8}>
+            <Space
+              direction="vertical"
+              size="small"
+              style={{
+                display: "flex",
+                marginTop: "60px",
+              }}
+            >
+              <hr style={{ color: "gray" }} />
+              <p>语种识别，输入需处理的文本，自动识别语言</p> <hr />
+              <p>
+                系统支持多语种处理，支持的语种多达17种，包括阿拉伯语、孟加拉语、缅甸语、汉语、英语等
+              </p>
+              <hr style={{ color: "gray" }} />
+              <p>
+                系统支持的计量指标也多达20种，包括有在不同语种之间通用的计量指标和语言特有指标
+              </p>
+              <hr style={{ color: "gray" }} />
+              <p>支持文档处理，用户可上传文档解析处理，更加人性化</p>{" "}
+              <hr style={{ color: "gray" }} />
+            </Space>
+          </Col>
+        </Row>
       </div>
     </>
   );
