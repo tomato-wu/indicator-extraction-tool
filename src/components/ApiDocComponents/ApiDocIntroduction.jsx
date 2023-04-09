549f30e513c906ccdd8aa225eb71838b1143ea7b
tomato <h20333@qq.com>
2023-04-09 20:59:27 +0800

    fix:不超过1M

--- src/components/ApiDocComponents/ApiDocIntroduction.jsx
+++ src/components/ApiDocComponents/ApiDocIntroduction.jsx
@@ -1,46 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Checkbox, message, Space, Menu } from "antd";
-import { getPersonApi, changeInformationApi } from "../../utils/axios/personal";
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  message,
+  Space,
+  Menu,
+  Typography,
+} from "antd";
+const { Title, Paragraph, Text, Link } = Typography;
 
 const PersonCardBox = {
-  width: "60vw",
-  height: "32vh",
-  borderRadius: "20px",
-  margin: "20px 120px",
-  boxShadow: "1px 1px 3px grey",
+  width: "100%",
+  height: "100%",
+  marginLeft: "100px",
+  padding: "20px",
   backgroundColor: "#FDFDFD",
-};
-
-const personTitleBox = {
-  height: "5vh",
-  backgroundColor: "rgb(244,246,251)",
-  borderRadius: "20px 20px 0 0",
-  lineHeight: "5vh",
-};
-
-const personTitleText = {
-  color: "black",
-  textShadow: "1px 1px 18px rgb(22,119,255)",
-  marginLeft: "20px",
-};
-
-const box = {
-  width: "60vw",
-  height: "20vh",
-  display: "flex",
-  marginLeft: "50px",
-  marginTop: "20px",
-  alignItems: "center",
 };
 
 const ApiDocIntroduction = () => {
   return (
     <>
       <div style={PersonCardBox}>
-        <div style={personTitleBox}>
-          <h3 style={personTitleText}>个人信息</h3>
-        </div>
-        <div style={box}>affadfd</div>
+        <Title level={2}>介绍</Title>
+        <Paragraph>
+          我们提供完善的设计原则、最佳实践和设计资源文件（
+          <Text code>Sketch</Text> 和<Text code>Axure</Text>
+          ），来帮助业务快速设计出高质量的产品原型。
+        </Paragraph>
       </div>
     </>
   );
--- src/components/ApiDocComponents/ChatWithUs.jsx
+++ src/components/ApiDocComponents/ChatWithUs.jsx
@@ -1,46 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Checkbox, message, Space, Menu } from "antd";
-import { getPersonApi, changeInformationApi } from "../../utils/axios/personal";
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  message,
+  Space,
+  Menu,
+  Typography,
+} from "antd";
+const { Title, Paragraph, Text, Link } = Typography;
 
 const PersonCardBox = {
-  width: "60vw",
-  height: "auto",
-  borderRadius: "20px",
-  margin: "20px 120px",
-  boxShadow: "1px 1px 3px grey",
+  width: "100%",
+  height: "100%",
+  marginLeft: "100px",
+  padding: "20px",
   backgroundColor: "#FDFDFD",
-};
-
-const personTitleBox = {
-  height: "5vh",
-  backgroundColor: "rgb(244,246,251)",
-  borderRadius: "20px 20px 0 0",
-  lineHeight: "5vh",
-};
-
-const personTitleText = {
-  color: "black",
-  textShadow: "1px 1px 18px rgb(22,119,255)",
-  marginLeft: "20px",
-};
-
-const box = {
-  color: "black",
-  padding: "0px",
 };
 
 const ChatWithUs = () => {
   return (
     <>
       <div style={PersonCardBox}>
-        <div style={personTitleBox}>
-          <h3 style={personTitleText}>联系我们</h3>
-        </div>
-        <div style={box}>
-          <h1>如需咨询，可通过下面的邮箱与我们取得联系：</h1>
-          <p>邮箱：translate_api@baidu.com</p>
-          <p>注：接入问题请优先参考接入文档。</p>
-        </div>
+        <Title level={3} style={{ marginBottom: "5vh" }}>
+          联系我们
+        </Title>
+
+        <Title level={4}>如需咨询，可通过下面的邮箱与我们取得联系：</Title>
+        <Paragraph>
+          邮箱：<Link href="https://mail.qq.com/">h20333@qq.com</Link>
+        </Paragraph>
+        <Paragraph>注：接入问题请优先参考接入文档。</Paragraph>
       </div>
     </>
   );
