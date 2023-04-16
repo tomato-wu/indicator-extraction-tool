3f3b1dadaf7230654bb4690bd51a2f634befcd63
jzy <3011543110@qq.com>
2023-04-16 19:10:59 +0800

    fix(multifile): 解决fileList更新问题

--- src/pages/multiDocumentProcessing.jsx
+++ src/pages/multiDocumentProcessing.jsx
@@ -1,108 +1,105 @@
-import { Steps, message, Upload, Select, Button, Checkbox } from "antd";
+import { Steps, message, Upload, Button } from 'antd'
 import {
   UploadOutlined,
   SyncOutlined,
   MonitorOutlined,
   SubnodeOutlined,
-  InboxOutlined,
-} from "@ant-design/icons";
-import React, { useState } from "react";
+} from '@ant-design/icons'
+import React, { useState } from 'react'
 
-import "./documentProcessing.css";
-import UploadFileInit from "../components/uploadFileComponent/UploadFileInit";
-import { getUploadFileListApi } from "../utils/axios/api";
+import './documentProcessing.css'
+import UploadFileInit from '../components/uploadFileComponent/UploadFileInit'
+import { getUploadFileListApi } from '../utils/axios/api'
 
 function MultiDocumentProcessing() {
   //最终上传的文件列表
-  const [fileList, setFileList] = useState([]);
-  const [uploading, setUploading] = useState(false);
+  const [fileList, setFileList] = useState([])
+  const [uploading, setUploading] = useState(false)
 
   const props = {
-    // 移除文件
-    onRemove: (file) => {
-      const index = fileList.indexOf(file);
-      const newFileList = fileList.slice();
-      newFileList.splice(index, 1);
-      setFileList(newFileList);
-      message.success("移除成功");
+    onChange: (arg) => {
+      const _fileList = arg.fileList
+      _fileList.length < fileList.length
+        ? setFileList((fileList) =>
+            fileList.filter((item) => item.uid !== arg.file.uid)
+          )
+        : setFileList((fileList) => fileList.concat(arg.file))
     },
-    // 文件上传前的钩子
-    beforeUpload: (file) => {
-      setFileList([...fileList, file]);
-      message.success("上传成功");
-      return false;
+    beforeUpload: (file, _fileList) => {
+      return false
     },
     fileList,
-  };
+  }
   // 上传文件列表
   const handleUpload = () => {
-    const formData = new FormData();
+    const formData = new FormData()
     fileList.forEach((file) => {
-      formData.append("files", file);
-    });
-    setUploading(true);
+      formData.append('files', file)
+    })
+    setUploading(true)
     getUploadFileListApi(formData).then((res) => {
-      if (res.code === 200) {
-        message.success("上传成功");
-        setUploading(false);
-        setFileList([]);
+      if (res?.code === 0) {
+        message.success('上传成功')
+        setUploading(false)
+        setFileList([])
       } else {
-        message.error("上传失败");
-        setUploading(false);
+        message.error('上传失败')
+        setUploading(false)
       }
-    });
-  };
+    })
+  }
 
   return (
     <>
       {/* 步骤条展示流程 */}
       <Steps
-        style={{ marginTop: "20px" }}
+        style={{ marginTop: '20px' }}
         current={0}
         items={[
           {
-            title: "依次上传文档",
-            status: "finish",
+            title: '依次上传文档',
+            status: 'finish',
             icon: <UploadOutlined />,
           },
           {
-            title: "可通过拖拽文档调整顺序",
-            status: "finish",
+            title: '可通过拖拽文档调整顺序',
+            status: 'finish',
             icon: <SyncOutlined />,
           },
           {
-            title: "提交处理",
-            status: "finish",
+            title: '提交处理',
+            status: 'finish',
             icon: <MonitorOutlined />,
           },
           {
-            title: "邮箱返回结果",
-            status: "finish",
+            title: '邮箱返回结果',
+            status: 'finish',
             icon: <SubnodeOutlined />,
           },
         ]}
       />
       {/* 文件上传 */}
-      <div className="cardBox" style={{ height: "auto" }}>
+      <div className="cardBox" style={{ height: 'auto' }}>
         <div className="titleBox">
           <h3 className="titleText">多文档上传</h3>
         </div>
         <div
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            paddingBottom: "40px",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            paddingBottom: '40px',
           }}
         >
           {/* 多文档上传 */}
           <Upload
             {...props}
+            multiple
             listType="picture"
             style={{
-              borderStyle: "none",
-              background: "white",
+              borderStyle: 'none',
+              background: 'white',
             }}
           >
             <UploadFileInit />
@@ -117,15 +114,15 @@ function MultiDocumentProcessing() {
         disabled={fileList.length === 0}
         loading={uploading}
         style={{
-          position: "relative",
-          left: "50%",
-          transform: "translateX(-50%)",
+          position: 'relative',
+          left: '50%',
+          transform: 'translateX(-50%)',
         }}
       >
-        {uploading ? "Uploading" : "确定上传"}
+        {uploading ? 'Uploading' : '确定上传'}
       </Button>
     </>
-  );
+  )
 }
 
-export default MultiDocumentProcessing;
+export default MultiDocumentProcessing
